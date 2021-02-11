e585e95a4e1619072f251360929d509064b15f75
Kacper1104 <kacper.kozimor@gmail.com>
2021-02-11 14:03:38 +0100

    build

---
+++ build/precache-manifest.34d3d91b17ef70f548ec6b6564f8d147.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8449dbbec4703efadeac5b1c35edf51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd1feedbb97770d3f5dd",
+    "url": "/static/css/2.f0419b90.chunk.css"
+  },
+  {
+    "revision": "9e5c2db17d2682ce5124",
+    "url": "/static/css/main.717b9f08.chunk.css"
+  },
+  {
+    "revision": "bd1feedbb97770d3f5dd",
+    "url": "/static/js/2.2e048bc8.chunk.js"
+  },
+  {
+    "revision": "78d6f181e73afa64faeafea064f52fea",
+    "url": "/static/js/2.2e048bc8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9e5c2db17d2682ce5124",
+    "url": "/static/js/main.83993c3b.chunk.js"
+  },
+  {
+    "revision": "29c4995013be4954e500",
+    "url": "/static/js/runtime-main.dcc7b5e5.js"
+  }
+]);
