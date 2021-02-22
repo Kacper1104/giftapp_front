eb1af273ca84ec7adee6721133231589ef1a4cc4
Kacper1104 <kacper.kozimor@gmail.com>
2021-02-23 00:21:39 +0100

    env names changes

--- src/Config/ServerConfig.js
+++ src/Config/ServerConfig.js
@@ -1,13 +1,14 @@
 import {} from "dotenv/config";
 
 const cloudinaryConfig = {
-	cloud_name: process.env.CLOUDINARY_CLOUD_NAME || "hwvc60mro",
-	api_key: process.env.CLOUDINARY_API_KEY || "637946998665293",
-	api_secret: process.env.CLOUDINARY_API_SECRET || "DF1n2ls3JpizChn1ha5j9m4ys8M"
+	cloud_name: process.env.REACT_APP_CLOUDINARY_CLOUD_NAME || "hwvc60mro",
+	api_key: process.env.REACT_APP_CLOUDINARY_API_KEY || "637946998665293",
+	api_secret:
+		process.env.REACT_APP_CLOUDINARY_API_SECRET || "DF1n2ls3JpizChn1ha5j9m4ys8M"
 };
 
 const config = {
-	server_address: process.env.BACKEND_URL || "http://localhost:6060",
+	server_address: process.env.REACT_APP_BACKEND_URL || "http://localhost:6060",
 	server_port: ""
 };
 
