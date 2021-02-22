5d994c1e0c5b06ea02c00932e7f4e0f848aa1921
Kacper1104 <kacper.kozimor@gmail.com>
2021-02-22 22:14:19 +0100

    cloudinary settings update

--- src/Config/ServerConfig.js
+++ src/Config/ServerConfig.js
@@ -1,24 +1,23 @@
 const cloudinaryConfig = {
-  cloud_name: 'hwvc60mro', 
-  api_key: '637946998665293', 
-  api_secret: 'DF1n2ls3JpizChn1ha5j9m4ys8M' 
+	cloud_name: process.env.CLOUDINARY_CLOUD_NAME || "hwvc60mro",
+	api_key: process.env.CLOUDINARY_API_KEY || "637946998665293",
+	api_secret: process.env.CLOUDINARY_API_SECRET || "DF1n2ls3JpizChn1ha5j9m4ys8M"
 };
 
 const config = {
-  server_address: process.env.BACKEND_URL || "http://localhost:6060",
-  server_port: "",
+	server_address: process.env.BACKEND_URL || "http://localhost:6060",
+	server_port: ""
 };
 
-
-const routes = { 
-  register: "/users", 
-  signIn: "/auth", 
-  auth: "/auth",
-  events: "/events", 
-  gifts: "/gifts", 
-  reservations: "/reservations", 
-  codes: "/codes",
-  joinEvent: "/events/join" 
+const routes = {
+	register: "/users",
+	signIn: "/auth",
+	auth: "/auth",
+	events: "/events",
+	gifts: "/gifts",
+	reservations: "/reservations",
+	codes: "/codes",
+	joinEvent: "/events/join"
 };
 
-module.exports = { config, routes, cloudinaryConfig};
+module.exports = { config, routes, cloudinaryConfig };
